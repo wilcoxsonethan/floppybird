f22da5fedc035320aa52f7a033a7845e3de9a6b1
lukeduncann <lukeduncansurf@gmail.com>
2018-01-17 08:46:40 -0800

    yuh yuh yuh v2

--- src/floppybird/Player.java
+++ src/floppybird/Player.java
@@ -1,4 +1,20 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 public class Player {
-
+private image floppyBird;
+private int xLocation = 0, yLocation = 0;
+public Bird (int initialWidth, int initialheight)
+{
+	floppyBird = Toolkit.getDefaultToolkit().
+	scaleBird(initialWidth, initialHeight);
+}
+public class Bird 
+{
+	private Image flappyBird;
+	private int xLocation = 0, yLocation = 0;
+}
 }
